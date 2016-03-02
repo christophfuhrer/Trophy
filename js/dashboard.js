c1e7c02e02045a0c0467d271e1eeb3bd27fe17c8
alexrauscher <alexanderrauscher@gmx.at>
2016-03-02 12:20:56 +0100

    Zusammenführung Fight, Training und Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+
+  /* 
+  /   Navigation zwischen Tower, Fight, Training und Trophies
+  */
   document.getElementById('nav_tower').addEventListener("click", clickNavLink);
 	document.getElementById('nav_fight').addEventListener("click", clickNavLink);
 	document.getElementById('nav_training').addEventListener("click", clickNavLink);
@@ -13,5 +17,23 @@ window.onload = function() {
   	}
  
   	document.getElementById(x).style.display = "block";
+  }
+
+  /* 
+  /   Navigation im Fight zwischen Funktionen, Vektoren und Textbeispielen
+  */
+  document.getElementById('btn_funktionen').addEventListener("click", functionClick);
+  document.getElementById('btn_vektoren').addEventListener("click", functionClick);
+  document.getElementById('btn_textbsp').addEventListener("click", functionClick);
+  
+  function functionClick(){
+    var x = this.getAttribute("id").replace("btn", "tab");
+    var allContent = document.querySelectorAll("div[id^='tab_']");
+
+    for (var i = 0; i < allContent.length; i++) {
+      allContent[i].style.display = "none";
+    }
+ 
+    document.getElementById(x).style.display = "block";
   }
 };
