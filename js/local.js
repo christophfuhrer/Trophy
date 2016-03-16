01a2a3df4b661ef783be68703ae27a1d22c92cae
Philipp Korkodilos <phil.korkodilos@gmail.com>
2016-03-16 11:15:50 +0100

    Finished Trophy-Room

--- js/local.js
+++ js/local.js
@@ -14,5 +14,17 @@ function getPointsInt(inter){
 		var a = document.getElementById("points");
 		a.textContent = "Punktestand: "+cur;
 		console.log("Aktuelle Punkte: " + cur);
+		
+		outputTrophies(1);
 	}, inter);
+}
+
+function outputTrophies(points){
+	var links = ["./images/trophies/gauss_trophy.svg", "./images/trophies/leibniz_trophy.svg", "./images/trophies/pythagoras_trophy.svg", "./images/trophies/qmark.png", "./images/trophies/qmark.png", "./images/trophies/qmark.png"];
+	for (var i = 0; i < points; i++) {
+		console.log("Cur Link: "+ links[i]);
+		var x = document.getElementsByClassName("trophy");
+		x[i].src = links[i];
+	}
+
 }
